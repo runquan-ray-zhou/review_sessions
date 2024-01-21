d6ece21c49c5d841361c03b0708894165c32204c
runquan-ray-zhou <darkraymond16@gmail.com>
2024-01-21 18:04:47 -0500

    add native array method

--- index_1_21.js
+++ index_1_21.js
@@ -9,14 +9,14 @@
 // Given an array of numbers and a given target. return the target if it's in the array or undefined if it's not
 
 function findTarget(arr, target) {
-    for (let num of arr) {
-        if (num === target) {
-            return num;
-        }
-    }
-    return undefined;
+    // for (let num of arr) {
+    //     if (num === target) {
+    //         return num;
+    //     }
+    // }
+    // return undefined;
 
-    // return arr.find((num) => num === target);
+    return arr.find((num) => num === target);
 }
 
 console.log(findTarget([1, 2, 3, 4, 5], 5))
@@ -26,12 +26,14 @@ console.log(findTarget([1, 2, 3, 4, 5],15))
 // Given an array of numbers, return the first number that's bigger than 14 or undefined if no number is bigger than 14
 
 function findBiggerThanFourteen(arr) {
-    for (let num of arr) {
-        if (num > 14) {
-            return num
-        }
-    }
-    return undefined;
+    // for (let num of arr) {
+    //     if (num > 14) {
+    //         return num
+    //     }
+    // }
+    // return undefined;
+
+    return arr.find(num => num > 14);
 }
 
 console.log(findBiggerThanFourteen([11, 12, 13, 14, 15]))
@@ -40,12 +42,14 @@ console.log(findBiggerThanFourteen([1, 2, 3, 4, 5]))
 // Given an array of strings, return the first string with length greater than 4 or undefined if no such string is found
 
 function findStringLengthGreaterThanFour (arr) {
-    for (let str of arr) {
-        if (str.length > 4) {
-            return str
-        }
-    }
-    return undefined;
+    // for (let str of arr) {
+    //     if (str.length > 4) {
+    //         return str
+    //     }
+    // }
+    // return undefined;
+
+    return arr.find(num => num.length > 4)
 }
 
 console.log(findStringLengthGreaterThanFour(["one", "two", "three", "four", "five"]))
