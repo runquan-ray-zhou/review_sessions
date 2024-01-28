511ec8225b4bb18b5144218f75b34b1ef7083f78
runquan-ray-zhou <darkraymond16@gmail.com>
2024-01-28 09:08:32 -0500

    add prompts to index01282024.js

---
+++ index01282024.js
@@ -0,0 +1,21 @@
+// Acc Pattern
+
+// Iterative Native Array Methods
+
+// For each of the following exercises, 
+// 1. write them using a for loop first
+// 2. comment out the accumulator pattern solution and write it using a native array method
+// 3. call the function and console.log the result
+
+// 1. Write a function that takes in an array of strings and returns a new array where each string is replaced by it's length
+// ["hello", "world"] => [5, 5] 
+
+// 2. Write a function that takes an array of strings and returns an array of strings longer than 5 characters (spaces count)
+
+// 3. Write a function that takes an array of strings, and returns a new array of the lengths of strings longer than 5 characters
+
+// 4. Write a function that takes an array of numbers and returns the sum of all even numebrs
+
+// 5. Write a function that takes an array of numbers and returns the min num
+
+// 6. Write a function that takes in a string and returns the character that appears the most times (only letters count)
